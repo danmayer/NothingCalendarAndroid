fb5907906237e73a7cc5e237292e9d739d99a472
Dan Mayer <dan@mayerdan.com>
2012-02-13 22:08:45 -0500

    table style changes

--- gen/com/mayerdan/nothingcalendar/R.java
+++ gen/com/mayerdan/nothingcalendar/R.java
@@ -26,5 +26,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int user_id=0x7f040003;
+        public static final int user_name=0x7f040002;
     }
 }
